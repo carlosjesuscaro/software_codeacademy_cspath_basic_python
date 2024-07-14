e495c8fbde3bbbcc9b38d5e5bf478a4841815b39
Carlos Jesus Caro <carlos.jesus.caro@gmail.com>
2024-07-14 09:37:01 -0400

    Importing TimedRotatingFileHandler

--- main.py
+++ main.py
@@ -3,9 +3,10 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-import logging
 import os
+import logging
 import logging.config
+from logging.handlers import TimedRotatingFileHandler
 from datetime import datetime
 from dotenv import find_dotenv, load_dotenv
 from src.strings_basic import count_unique_letters
