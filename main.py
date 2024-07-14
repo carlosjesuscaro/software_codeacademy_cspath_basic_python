ee28ad97b9c5ec94bba8a964b08e4c0cfa87a3a9
Carlos Jesus Caro <carlos.jesus.caro@gmail.com>
2024-07-14 11:31:44 -0400

    fix: logging configuration file updated

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ def setup_logging():
     config = log_configs.get(os.environ["ENV"])
     config_path = "/".join([CONFIG_DIR, config])
 
-    timestamp = datetime.now().strftime("%Y%m%d-%H:%M:%S")
+    # timestamp = datetime.now().strftime("%Y%m%d-%H:%M:%S")
+    timestamp = datetime.now().strftime("%Y%m%d")
 
     logging.config.fileConfig(
         config_path,
@@ -32,14 +33,13 @@ def setup_logging():
         defaults={"logfilename": f"{LOG_DIR}/{timestamp}.log"},
     )
 
-
 def string_challenge() -> None:
     """Runs all the exercises defined in the string challenge
     Args: None
     Returns: None
     Raises: None
     """
-    count_unique_letters(2242)
+    count_unique_letters('carlos')
     return None
 
 
