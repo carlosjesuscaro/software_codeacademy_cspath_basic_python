cc7b13340e72f835e9023b7719ec9c2e6c606275
Carlos Jesus Caro <carlos.jesus.caro@gmail.com>
2024-07-14 13:01:28 -0400

    fix: removing unnecesary comments

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
 import os
 import logging
 import logging.config
-from logging.handlers import TimedRotatingFileHandler
 from datetime import datetime
 from dotenv import find_dotenv, load_dotenv
 from src.strings_basic import count_unique_letters
